090dcaa2e2098da2bcb5ac207c14963936845566
an244 <thienan.le244@gmail.com>
2017-10-11 16:42:24 +0700

    simple emit & on

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const io = require("socket.io")(server);
 server.listen(3000,()=>console.log('server started'));
 
 //io lang nghe su kien ket noi hoặc ngắt kết nối giữa client và server
+//tất cả các emit từ client sẽ dc 'on' ở chỗ này và có 1 socket riêng với message riêng
 io.on("connection", socket =>{
     //mỗi khi có 1 kết nối lên server đều tạo 1 socket riêng biệt khác nhau cho từng kết nối
     console.log('co nguoi ket noi: '+ socket.id);
@@ -22,6 +23,12 @@ io.on("connection", socket =>{
   socket.on('disconnect', () =>{
    console.log('dã Thoát: ' + socket.id ); 
   });
+
+  //'Client-send-data' phải khớp với 'emit' bên home.ejs
+  socket.on('Client-send-data', (data)=>{
+    console.log(data);
+  });
+
 });
 
 app.get('/',(req,res)=> {
