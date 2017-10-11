46f360057fa3cbd6277df360059e88942a74976a
an244 <thienan.le244@gmail.com>
2017-10-11 12:50:51 +0700

    socketio init continue

--- index.js
+++ index.js
@@ -13,9 +13,15 @@ const io = require("socket.io")(server);
 //server lang nghe port 3000
 server.listen(3000,()=>console.log('server started'));
 
-//io lang nghe su kien ket noi
+//io lang nghe su kien ket noi hoặc ngắt kết nối giữa client và server
 io.on("connection", socket =>{
-    console.log('co nguoi ket noi');
+    //mỗi khi có 1 kết nối lên server đều tạo 1 socket riêng biệt khác nhau cho từng kết nối
+    console.log('co nguoi ket noi: '+ socket.id);
+
+  //socket lắng nghe sự kiện ngắt kết nối
+  socket.on('disconnect', () =>{
+   console.log('dã Thoát: ' + socket.id ); 
+  });
 });
 
 app.get('/',(req,res)=> {
